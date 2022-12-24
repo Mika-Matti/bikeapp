5ebbc14e4fe999801d85d61dc3ad887023f3c75b
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2022-12-24 16:15:53 +0200

    Add server base

---
+++ router.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+//Db tables and columns
+//TODO
+
+//DB Connection
+//TODO
+
+//Connect to DB
+//TODO
+
+// Default page
+router.get("/", function (req, res) {
+  res.send("Hello");
+});
+
+// CREATE, READ, EDIT, DELETE functions
+
+// Get items
+//TODO
+
+// Add item
+//TODO
+
+// Update item
+//TODO
+
+// Delete item
+//TODO
+
+module.exports = router;
