12f4b71eabab5dde992cfd3d07afe3302fd00e5a
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2023-01-23 06:26:02 +0200

    Add bootstrap styles to journey rows

--- client/src/components/Journeynode.js
+++ client/src/components/Journeynode.js
@@ -3,19 +3,49 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 export default class Journeynode extends Component {
+  parseDate = (date) => {
+    date = String(date).split(" ");
+    const days = String(date[0]).split("-");
+    return [parseInt(days[2]), "/", parseInt(days[1]), "/", parseInt(days[0])];
+  };
+
   render() {
-    const { journey_id, departure_time, return_time } = this.props.journey;
+    const {
+      journey_id,
+      departure_time,
+      return_time,
+      departure_station_id,
+      departure_station_name,
+      return_station_id,
+      return_station_name,
+      journey_distance,
+      journey_duration,
+    } = this.props.journey;
 
     return (
       <div className="row mx-0 border-top pt-2">
-        <div className="col-1">
+        <div className="col-1 text-left">
           <b>{journey_id}</b>
         </div>
-        <div className="col-3">{departure_time}</div>
-        <div className="col-3">{return_time}</div>
-        <div className="col-2">Distance</div>
-        <div className="col-2">Duration</div>
-        <div className="col-1">
+        <div className="col-9 px-0 text-left">
+          <div className="row mx-0">
+            <div className="col-sm-4 col-md-2">
+              {this.parseDate(departure_time)}
+            </div>
+            <div className="col-sm-4 col-md-2 text-break">
+              {departure_station_name}
+            </div>
+            <div className="col-sm-4 col-md-2">
+              {this.parseDate(return_time)}
+            </div>
+            <div className="col-sm-4 col-md-2 text-break">
+              {return_station_name}
+            </div>
+            <div className="col-sm-4 col-md-2">{journey_distance}</div>
+            <div className="col-sm-4 col-md-2">{journey_duration}</div>
+          </div>
+        </div>
+        <div className="col-2 text-left">
           <Link
             type="button"
             className="btn btn-dark"
@@ -30,5 +60,5 @@ export default class Journeynode extends Component {
 }
 
 Journeynode.propTypes = {
-  Journey: PropTypes.object.isRequired,
+  journey: PropTypes.object.isRequired,
 };
--- client/src/components/Journeys.js
+++ client/src/components/Journeys.js
@@ -8,9 +8,15 @@ export default class Journeys extends Component {
     this.state = {
       journeys: [
         {
-          journey_id: 1,
+          journey_id: 0,
           departure_time: "",
           return_time: "",
+          departure_station_id: 0,
+          departure_station_name: "",
+          return_station_id: 0,
+          return_station_name: "",
+          journey_distance: 0,
+          journey_duration: 0,
         },
       ],
     };
@@ -24,25 +30,35 @@ export default class Journeys extends Component {
           return (
             <div className="container">
               {/* header */}
-              <h1 className="display-5 my-3 text-left">Journeys</h1>
+              <h1 className="my-3 text-left">Journeys</h1>
               {/* column headers for rows*/}
               <div className="row mx-0 border-top py-2">
-                <div className="col-1">
+                <div className="col-1 text-left">
                   <b>#</b>
                 </div>
-                <div className="col-3">
-                  <b>Departed</b>
+                <div className="col-9 px-0 text-left">
+                  <div className="row mx-0">
+                    <div className="col-sm-4 col-md-2 ">
+                      <b>Departed</b>
+                    </div>
+                    <div className="col-sm-4 col-md-2 ">
+                      <b>Departure station</b>
+                    </div>
+                    <div className="col-sm-4 col-md-2">
+                      <b>Returned</b>
+                    </div>
+                    <div className="col-sm-4 col-md-2">
+                      <b>Return station</b>
+                    </div>
+                    <div className="col-sm-4 col-md-2">
+                      <b>Distance {"(meters)"}</b>
+                    </div>
+                    <div className="col-sm-4 col-md-2">
+                      <b>Duration {"(seconds)"}</b>
+                    </div>
+                  </div>
                 </div>
-                <div className="col-3">
-                  <b>Returned</b>
-                </div>
-                <div className="col-2">
-                  <b>Distance</b>
-                </div>
-                <div className="col-2">
-                  <b>Duration</b>
-                </div>
-                <div className="col-1">
+                <div className="col-2 text-left">
                   <b>More</b>
                 </div>
               </div>
